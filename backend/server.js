fc56ccc5f0b0501fbf006dad206ba8fbadd25954
kosal1986 <kosalpoesr@gmail.com>
2023-11-25 05:14:10 +0700

    "Connect to DB"

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || "3500";
 
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connect to mongoDb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(PORT, () => {
